8e6f31c029889a2e81c4d3fcd9cefd86486498dd
Kilian Lieret <kilian.lieret@posteo.de>
2025-08-28 18:33:42 -0400

    CI: Test single player mode as well

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,9 +6,17 @@ using DeterministicModel instead of real LLM models.
 """
 
 from codeclash import CONFIG_DIR
-from main import main_cli
 
 
 def test_pvp_battlesnake():
+    from main import main_cli
+
     config_path = CONFIG_DIR / "test_configs" / "battlesnake_pvp_test.yaml"
+    main_cli(["-c", str(config_path)])
+
+
+def test_single_player_battlesnake():
+    from main_single_player import main_cli
+
+    config_path = CONFIG_DIR / "test_configs" / "battlesnake_single_player_test.yaml"
     main_cli(["-c", str(config_path)])
